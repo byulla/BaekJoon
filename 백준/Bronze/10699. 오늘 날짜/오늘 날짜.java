c9651ce8cb2b52f8f3c9fdb92ed45f1addfc80fb
Jo Hyera <s2037@e-mirim.hs.kr>
2022-04-06 15:02:25 +0900

    [Bronze V] Title: 오늘 날짜, Time: 116 ms, Memory: 13404 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// 현재 날짜 구하기 (시스템 시계, 시스템 타임존)
+		LocalDate now = LocalDate.now();
+		
+		System.out.println(now);
+		
+
+	}
+
+}

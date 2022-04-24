5bb2ca1ea1bd2ae8cc738ba4190983ab9abfbb4c
Cho Hyera <s2037@e-mirim.hs.kr>
2022-04-24 16:35:45 +0900

    [Bronze II] Title: 단어 길이 재기, Time: 104 ms, Memory: 12772 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		
+		String str = scan.next();
+		int len = str.length();
+		
+		System.out.println(len);
+
+	}
+
+}
